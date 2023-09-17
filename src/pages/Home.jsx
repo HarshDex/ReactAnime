721e47a003d2db8f9fefc439fb96db01d7b206a5
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-09-18 04:36:37 +0530

    added vignette

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-        <div className="background-image">
-            <img src="/src/assets/flat.png" alt="" />
-        </div>
-        <div className="heading">
-            AnimeFlix
-        </div>
-        
+      <div className="background-image">
+        <img src="/src/assets/flat.png" alt="" />
+      </div>
+      <div className="heading">
+        <h1> GhibliFlix</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
