84fd1a74ccf5359fe6a29d376c81712fc7f68ed4
HarshDex <nature4917@gmail.com>
2023-09-18 02:50:43 +0530

    animePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./pages/Login";
+import Movies from "./pages/Movies";
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Movies/>
     </>
   );
 }
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Movies.css"
+const Movies = () => {
+  return (
+    <div className="movie-page">
+        <div className="movie-description-part">
+            <div id="des-about">
+                <div id="des-about-head">
+                    <h1>Tokyo Revengers : Christmas ShowDown</h1>
+                </div>
+                <div id="des-tags">
+                    <div className="duration" class = "tags">
+                        <i class="ri-star-fill"></i>
+                        9.5
+                    </div>
+                    <div className="rating" class = "tags">
+                        <i class="ri-time-line"></i>
+                        121m
+                    </div>
+                </div>
+                <div id="des-description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae ab temporibus nisi dicta ex tenetur nesciunt velit error unde fugiat inventore, ut beatae ipsum quam illo reiciendis nam fuga modi maxime iusto itaque quidem officiis in. Iste obcaecati ratione aperiam quaerat natus esse sit aut, in a! Animi, unde ducimus.
+                </div>
+            </div>
+            <div id="des-image">
+                <img src="https://images2.alphacoders.com/132/1323264.jpeg" alt="" />
+            </div>
+        </div>
+        <button className='button'>
+            <i class="ri-play-line"></i>            
+            Watch Now
+        </button>
+        <button className = "button1">
+        <i class="ri-add-line"></i>
+            Add to list</button>
+    </div>
+  )
+}
+
+export default Movies
